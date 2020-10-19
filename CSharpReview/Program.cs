c8b31a8d5c2d8881df33e91a977bbf08cbbeec77
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-19 10:02:58 -0600

    Variables review; Output to the console;

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace CSharpReview
 {
     class Program
     {
         static void Main(string[] args)
+            // Entry point of programs in C#.
         {
-            Console.WriteLine("Hello World! Be happy!");
+            // Declaration of variables
+
+            char aChar;
+            string aString;
+
+            bool aBoolean;
+
+            byte aByte; // Super small integer values (~120 max)
+            int anInt;
+            short aShortInt;
+            long aLongInt;
+            
+            float aFloat; // Less precise than double
+            double aDouble;
+            decimal aDecimal; // Extremely precise
+
+
+            string name;
+
+            name = Console.ReadLine();
+
+            // Concatenation
+            Console.WriteLine("Hello "+name+"! What a nice day!");
+            // Parameterized output
+            Console.WriteLine("Hello {0}! What a nice day today!", name);
+            // String interpolation
+            Console.WriteLine($"Hello {name}! What a nice day today!");
         }
     }
 }
