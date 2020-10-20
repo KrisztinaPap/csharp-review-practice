cad9564fd3af4ce9e07615353e5b7128f5a5f3a6
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-20 09:37:43 -0600

    Removed systems I don't use/need;

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.VisualBasic.CompilerServices;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
 
 namespace CSharpReview
 {
