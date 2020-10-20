29f016dd13827a0381d931280e5e64c7e8c2a15d
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-20 10:39:09 -0600

    Adds validation loop ( try-catch block); Sentinel value loop;

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -1,38 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpReview
 {
     class Program
     {
         static void Main(string[] args)
-        // Entry point of programs in C#.
+        
         {
-            List<int> intList = new List<int>();
+            List<string> stringList = new List<string>();
 
-            int userInput = 1;
+            string userInput = "default";
             do
             {
-                userInput = GetInt("Please enter a positive integer to add to the list, or a negative one to exit: ");
-                if (userInput > 0)
+                userInput = GetName("Please enter a name to add to the list, or nothing (empty string) to exit: ");
+                if (userInput != "")
                 {
-                    intList.Add(userInput);
+                    if (stringList.Contains(userInput))
+                    {
+                        Console.WriteLine("That's a duplicate entry! Please try again!");
+                    } 
+                    else
+                    {
+                        stringList.Add(userInput);
+                    }                    
                 }
-            } while (userInput > 0);
+            } while (userInput != "");
 
-            foreach ( int i in intList)
+            foreach ( string name in stringList )
             {
-                Console.WriteLine(i);
+                Console.WriteLine(name);
             }
+        }
+
+        static string GetName(string prompt)
+        {
+            string input;
+            Console.Write(prompt);
+            input = Console.ReadLine().Trim().ToLower();
+            return input;
         }
 
         static int GetInt(string prompt)
         {
-            int input;
-            Console.Write(prompt);
-            input = int.Parse(Console.ReadLine());
+            int input = 0;
+            bool valid = false;
+            do
+            {
+                Console.Write(prompt);
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (!valid);
+
             return input;
         }
-        
+
     }
 }
